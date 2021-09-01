e01074589061fe1f66833221026cf66ad7b88cf2
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2021-09-02 05:14:26 +0600

    full done

--- js/api.js
+++ js/api.js
@@ -2,6 +2,8 @@ const searchText = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
 const resultDiv = document.getElementById('result');
 const viewDetails = document.getElementById('view-details');
+const spinner = document.getElementById('spinner');
+const errorDiv = document.getElementById('error');
 
 /* const url = `https://covers.openlibrary.org/b/id/{cover_i}-M.jpg`;
     fetch(url)
@@ -11,20 +13,46 @@ const viewDetails = document.getElementById('view-details');
 // search book 
 searchBtn.addEventListener('click', function () {
     const search = searchText.value;
+
+    //clear   
+    clearFields();
+
+    //empty search field handel
+    if (search === '') {
+        errorDiv.innerText = 'Search field can not be empty!';
+        return;
+    }
     // fetch data
     const url = `http://openlibrary.org/search.json?q=${search}`;
+
+    //showing spinner
+    spinner.classList.remove('d-none');
+
     fetch(url)
         .then(res => res.json())
-        .then(data => showResults(data));
+        .then(data => {
+            setTimeout(() => {
+                spinner.classList.add('d-none');
+                showResults(data);
+            }, 50);
+        })
+        .finally(() => {
+            searchText.value = '';
+        });
+
 });
 
 function showResults(dataArray) {
+    // error handel
+    if (dataArray.numFound === 0) {
+        errorDiv.innerText = 'No result found!'
+    }
     /* resultDiv.innerText = `Search found: ${dataArray.numFound}`;
     console.log(dataArray); */
 
     const allData = dataArray.docs;
-    /* const dataLength = allData.length;
-    console.log(dataLength); */
+    const dataLength = allData.length;
+    resultDiv.innerText = `Search found: ${dataLength}`;
 
     allData.slice(0, 10).forEach(item => {
         console.log(item.text[2], item.text[3]);
@@ -47,4 +75,10 @@ function showResults(dataArray) {
         `;
         viewDetails.appendChild(div);
     });
+}
+
+function clearFields() {
+    viewDetails.innerHTML = '';
+    errorDiv.innerText = '';
+    resultDiv.innerText = '';
 }
