81352b90fa95c93a9d9e88f7867d25ab61a7f0f3
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2021-09-02 22:25:21 +0600

    final version

--- js/api.js
+++ js/api.js
@@ -5,11 +5,6 @@ const viewDetails = document.getElementById('view-details');
 const spinner = document.getElementById('spinner');
 const errorDiv = document.getElementById('error');
 
-/* const url = `https://covers.openlibrary.org/b/id/{cover_i}-M.jpg`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => showResults(data)); */
-
 // search book 
 searchBtn.addEventListener('click', function () {
     const search = searchText.value;
@@ -23,7 +18,7 @@ searchBtn.addEventListener('click', function () {
         return;
     }
     // fetch data
-    const url = `http://openlibrary.org/search.json?q=${search}`;
+    const url = `https://openlibrary.org/search.json?q=${search}`;
 
     //showing spinner
     spinner.classList.remove('d-none');
@@ -47,15 +42,15 @@ function showResults(dataArray) {
     if (dataArray.numFound === 0) {
         errorDiv.innerText = 'No result found!'
     }
-    /* resultDiv.innerText = `Search found: ${dataArray.numFound}`;
-    console.log(dataArray); */
 
+    // show search found result
     const allData = dataArray.docs;
     const dataLength = allData.length;
     resultDiv.innerText = `Search found: ${dataLength}`;
 
+    //show search result on UI
     allData.slice(0, 10).forEach(item => {
-        console.log(item.text[2], item.text[3]);
+
         const div = document.createElement('div');
         div.classList.add('col-md-3');
         div.innerHTML = `
@@ -65,10 +60,7 @@ function showResults(dataArray) {
                     <div class="card-body">
                         <h5 class="card-title"><i>Book title:</i> ${item.title}</h5>
                         <hr>
-                        <small>${item.text[2]}</small>
-                        <small>${item.text[3]}</small>
-                        <hr>
-                        <p class="card-text"><b>Author:</b> ${item.author_name[0]}</p>
+                        <p class="card-text"><b>Author:</b> ${item.author_name}</p>
                         <p class="card-text"><b>Publisher:</b> ${item.publisher_facet[0]}</p>
                         <p class="card-text"><b>First published:</b> ${item.first_publish_year}</p>
                     </div>
@@ -77,8 +69,11 @@ function showResults(dataArray) {
     });
 }
 
-function clearFields() {
+const clearFields = () => {
     viewDetails.innerHTML = '';
     errorDiv.innerText = '';
     resultDiv.innerText = '';
 }
+
+
+
